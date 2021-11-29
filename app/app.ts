bacdceb926235acabb49625b4321f0c3cff091ea
ThiagoCalisto <warlocalgames@gmail.com>
2021-11-29 18:39:56 -0300

    inicializando projeto

---
+++ app/app.ts
@@ -0,0 +1,8 @@
+import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { Negociacao } from "./models/Negociacao.js";
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event =>{
+    event.preventDefault();
+    controller.adiciona();
+})
